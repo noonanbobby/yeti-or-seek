004f275dcdd92e574ee47e62593e18e2e8bf0705
noonanbobby <95311481+noonanbobby@users.noreply.github.com>
2025-08-11 13:56:29 -0700

    Create GameMode.cs

---
+++ Assets/Scripts/Core/GameMode.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// GameMode defines the different core game types available in Yeti Or Seek.
+/// Race mode is a fast‑paced obstacle course where players compete to finish first.
+/// HideSeek mode is a twist on hide and seek: some players hide as objects while
+/// others seek. Additional modes can be added here in future updates.
+/// </summary>
+public enum GameMode
+{
+    Race,
+    HideSeek
+}
